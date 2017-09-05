a81600cd7200b5065ddbc78e1c7894f461417764
Vlets <georgevle93@gmail.com>
2017-09-05 09:47:20 +0200

    5/9/2017 9:46am
    Workshop task 1 working, test failing on task 2

--- Lab1Solutions.hs
+++ Lab1Solutions.hs
@@ -1,7 +1,38 @@
+--Setup
+
+module Lab1Answers where
+import Data.List
+import Test.QuickCheck
+
+prime :: Integer -> Bool
+prime n = n > 1 && all (\ x -> rem n x /= 0) xs
+  where xs = takeWhile (\ y -> y^2 <= n) primes
+
+primes :: [Integer]
+primes = 2 : filter prime [3..]
+
+infix 1 --> 
+
+(-->) :: Bool -> Bool -> Bool
+p --> q = (not p) || q
+
+forall :: [a] -> (a -> Bool) -> Bool
+forall = flip all
+
+--Workshop exercise 1 as a test (Working) took about 2 hours
+
 f1, f2 :: Int -> Int
 f1 = \ n -> sum [0..n]
-f2 = \ n -> (n*(n+1)) / 2
+f2 = \ n -> (n*(n+1)) `div` 2
 
-test1 = quickCheckResult (\ n -> n >= 0 --> f1 n == f2 n)
+test1 = quickCheckResult (\n -> n>=0 --> f1 n == f2 n)
 
---Not working
+--Workshop exercise 2 (Task 1)
+
+f11, f21  :: Int -> Int
+f11 = \ n -> sum[1^2..n^2]
+f21 = \ n -> (n*(n+1)*(2*n+1)) `div` 6
+
+test2 = quickCheckResult (\n -> n>=0 --> f11 n == f21 n)
+
+
