c9f420e48580a89d0255655cbdf772df1239167e
TerryvanWalen <github@terryvanwalen.nl>
2017-09-05 10:45:45 +0200

    Lab1 - Exercise 1 - 2

--- Lab1/10428232-TerryVanWalen/Lab1.hs
+++ Lab1/10428232-TerryVanWalen/Lab1.hs
@@ -10,12 +10,16 @@ p --> q = (not p) || q
 forall :: [a] -> (a -> Bool) -> Bool
 forall = flip all
 
-power2 :: [Int] -> [Int]
-power2 [] = []
-power2 (x:xs) = (x^2) : (power2 xs)
+power :: Int -> [Int] -> [Int]
+power p [] = []
+power p (x:xs) = (x^p) : (power p xs)
 
 f21, f22 :: Int -> Int
-f21 = \n -> sum (power2 [0..n])
+f21 = \n -> sum (power 2 [0..n])
 f22 = \n -> div (product [n, n+1,2*n + 1]) 6
-
 test2 = quickCheckResult (\n -> n >= 0 --> f21 n == f22 n)
+
+f31, f32 :: Int -> Int
+f31 = \n -> sum (power 3 [0..n])
+f32 = \n -> (div (product [n, n+1]) 2)^2
+test3 = quickCheckResult (\n -> n >= 0 --> f31 n == f32 n)
