2ff3477ecde0da41b05b194c664943ebc868a597
TerryvanWalen <github@terryvanwalen.nl>
2017-09-11 14:41:55 +0200

    Starting the second exercise

--- Lab2/10428232-TerryVanWalen/Lab2.hs
+++ Lab2/10428232-TerryVanWalen/Lab2.hs
@@ -10,6 +10,9 @@ infix 1 -->
 (-->) :: Bool -> Bool -> Bool
 p --> q = (not p) || q
 
+
+-- Exercise 1 : 45 minutes
+-- Tested with 1.000.000 and the proportions are as expected.
 probs :: Int -> IO [Float]
 probs 0 = return []
 probs n = do
@@ -21,8 +24,12 @@ divideInQuartets :: Int -> IO [Int]
 divideInQuartets l = do
   x <- probs l
   let q1 = length (filter (<0.25) x)
-  let q2 = length (filter (<0.50 && >0.25) x) - q1
+  let q2 = length (filter (<0.50) x) - q1
   let q3 = length (filter (<0.75) x) - (q1 + q2)
   let q4 = length (filter (<1.0) x) - (q1 + q2 + q3)
   let q = [q1, q2 , q3, q4]
   return q
+
+
+
+-- Excercise 2 : ??
