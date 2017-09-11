856b22e8ed633097e023ff59a1c9f6cde381af1b
TerryvanWalen <github@terryvanwalen.nl>
2017-09-11 14:37:28 +0200

    First exercise

--- Lab2/10428232-TerryVanWalen/Lab2.hs
+++ Lab2/10428232-TerryVanWalen/Lab2.hs
@@ -1,1 +1,28 @@
-test
+module Lab2 where
+import Data.List
+import Data.Char
+import System.Random
+import Test.QuickCheck
+
+
+
+infix 1 -->
+(-->) :: Bool -> Bool -> Bool
+p --> q = (not p) || q
+
+probs :: Int -> IO [Float]
+probs 0 = return []
+probs n = do
+    p <- getStdRandom random
+    ps <- probs (n-1)
+    return (p:ps)
+
+divideInQuartets :: Int -> IO [Int]
+divideInQuartets l = do
+  x <- probs l
+  let q1 = length (filter (<0.25) x)
+  let q2 = length (filter (<0.50 && >0.25) x) - q1
+  let q3 = length (filter (<0.75) x) - (q1 + q2)
+  let q4 = length (filter (<1.0) x) - (q1 + q2 + q3)
+  let q = [q1, q2 , q3, q4]
+  return q
