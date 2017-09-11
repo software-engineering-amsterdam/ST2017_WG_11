4e07f0ff8f1fca52603b48b1f6a318db5820abde
TerryvanWalen <github@terryvanwalen.nl>
2017-09-11 15:06:33 +0200

    Exercise basic 2

--- Lab2/10428232-TerryVanWalen/Lab2.hs
+++ Lab2/10428232-TerryVanWalen/Lab2.hs
@@ -33,3 +33,22 @@ divideInQuartets l = do
 
 
 -- Excercise 2 : ??
+data Shape = NoTriangle | Equilateral
+            | Isosceles  | Rectangular | Other deriving (Eq,Show)
+triangle :: Integer -> Integer -> Integer -> Shape
+triangle x y z   | x <= 0
+                   || y <= 0
+                   || z <= 0                    = NoTriangle
+                 | x >= y + z
+                   || y >= x + z
+                   || z >= x + y                = NoTriangle
+                 | x == y
+                   && x == z
+                   && y == z                    = Equilateral
+                 | x^2 + y^2 == z^2
+                   || x^2 + z^2 == y^2
+                   || y^2 + z^2 == x^2          = Rectangular
+                 | x == y
+                   || x == z
+                   || y == z                    = Isosceles
+                 | otherwise                    = Other
