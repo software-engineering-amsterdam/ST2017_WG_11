15bf3804b42eca5409f437294d08e63628bbb803
TerryvanWalen <github@terryvanwalen.nl>
2017-09-12 13:00:57 +0200

    Exercise 3

--- Lab2/10428232-TerryVanWalen/Lab2.hs
+++ Lab2/10428232-TerryVanWalen/Lab2.hs
@@ -5,11 +5,12 @@ import System.Random
 import Test.QuickCheck
 
 
-
 infix 1 -->
 (-->) :: Bool -> Bool -> Bool
 p --> q = (not p) || q
 
+forall :: [a] -> (a -> Bool) -> Bool
+forall = flip all
 
 -- Exercise 1 : 45 minutes
 -- Tested with 1.000.000 and the proportions are as expected.
@@ -32,7 +33,9 @@ divideInQuartets l = do
 
 
 
--- Excercise 2 : ??
+-- Excercise 2 : 30 minutes
+-- To test this we could generate a list of triples and filter them with the triangle function.
+-- In this list we could check some outputs but I see no way how to test this.
 data Shape = NoTriangle | Equilateral
             | Isosceles  | Rectangular | Other deriving (Eq,Show)
 triangle :: Integer -> Integer -> Integer -> Shape
@@ -52,3 +55,47 @@ triangle x y z   | x <= 0
                    || x == z
                    || y == z                    = Isosceles
                  | otherwise                    = Other
+
+
+
+-- Exercise 3 : 4 hours
+-- There are only 4 individual properties
+p1, p2, p3, p4 :: Integer -> Bool
+p1 = \n -> even n && n > 3
+p2 = \n -> even n
+p3 = \n -> even n || n > 3
+p4 = \n -> (even n && n > 3) || even n
+
+testP12 = stronger [(-10)..10] p1 p2
+testP32 = stronger [(-10)..10] p3 p2
+testP42 = stronger [(-10)..10] p4 p2
+testP24 = stronger [(-10)..10] p2 p4
+
+-- Quicksort the list
+test3 = quicksort' [fp1, fp2, fp3, fp4]
+
+stronger, weaker :: [a] -> (a -> Bool) -> (a -> Bool) -> Bool
+stronger xs p q = forall xs (\ x -> p x --> q x)
+weaker   xs p q = stronger xs q p
+
+quicksort' :: [FunctionPair a b] -> [String]
+quicksort' [] = []
+quicksort' (x:xs) =
+  quicksort' [y | y <- xs, (stronger [(-10)..10] (prop y) (prop x))]
+  ++ [(name x)]
+  ++ quicksort' [y | y <- xs, not (stronger [(-10)..10] (prop y) (prop x))]
+
+-- Nicolae told me about functionpairs. This took a long time to figure out.
+data FunctionPair a b = FunctionPair {
+    name :: String
+    , prop :: (Integer->Bool)
+}
+
+fp1 = FunctionPair "p1" p1
+fp2 = FunctionPair "p2" p2
+fp3 = FunctionPair "p3" p3
+fp4 = FunctionPair "p4" p4
+
+
+-- descendingStrengthList
+-- sortBy stronger plist
